2fe1adea06a18586f718d6cd6c25ec85411d2d86
Shivam <prajapatishivam11203@gmail.com>
2025-06-04 13:32:38 +0530

    feat: add material ui, constant file and setup react-router-dom

---
+++ client/lib/constant.js
@@ -0,0 +1,8 @@
+// Add constant data to map over
+export const CONSTANTS = {
+  // Define the constants for the application
+  APP_NAME: 'E-canteen System', // placeholder for the application name, it will be changed later
+  APP_DESCRIPTION:
+    'A modern e-canteen system for efficient food ordering and management in colleges.',
+  VERSION: '1.0.0',
+};
