fbdc484d3df93528b468d62031e5515d591345f1
Ritesh More <moreritesh028@gmail.com>
2025-06-05 20:54:27 +0530

    adding tab changes in forms

--- client/src/pages/Google.jsx
+++ client/src/pages/Google.jsx
@@ -1,19 +1,15 @@
-import {
+ import {
   SignIn,
-  SignUp,
   SignedIn,
   SignedOut,
   UserButton,
 } from "@clerk/clerk-react";
-
 import { useUser } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Google() {
   const collegename = localStorage.getItem("collegeName");
-  console.log(collegename);
-
   const { user, isSignedIn } = useUser();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -33,27 +29,50 @@ function Google() {
           console.log(response);
         } catch (error) {
           console.log(error);
-        } finally {
         }
       }
+
       sendDetails();
     }
   }, [user, isSignedIn, collegename]);
 
   return (
-    <div>
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      
+        padding: "1rem",
+      }}
+    >
       <SignedOut>
         <SignIn redirectUrl={window.location.pathname} />
       </SignedOut>
 
       <SignedIn>
-        <p>
-          You are signed in!
-          {collegename} <br />
-          {name} <br />
-          {email} <br />
-        </p>
-        <UserButton afterSignOutUrl="/register" />
+        <div
+          style={{
+        
+            padding: "2rem",
+            borderRadius: "12px",
+            boxShadow: "0 8px 24px rgba(0,0,0,0.1)",
+            textAlign: "center",
+            maxWidth: "400px",
+            width: "100%",
+          }}
+        >
+          <p>
+            ✅ You are signed in! <br />
+            <strong>College:</strong> {collegename} <br />
+            <strong>Name:</strong> {name} <br />
+            <strong>Email:</strong> {email}
+          </p>
+          <div style={{ marginTop: "1rem" }}>
+            <UserButton afterSignOutUrl="/register" />
+          </div>
+        </div>
       </SignedIn>
     </div>
   );
