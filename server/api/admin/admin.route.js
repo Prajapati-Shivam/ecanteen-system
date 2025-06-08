7ba9254e1de2a9e0e58a66eb50da4d602700ad63
DirrtyCoder <siddharthsurve29@gmail.com>
2025-06-08 13:41:42 +0530

    Server Restructured as mvc

--- server/api/admin/admin.route.js
+++ server/api/admin/admin.route.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const adminController = require('./admin.controller');
+const adminController = require("./admin.controller");
 
-router.post('/addItem', adminController.addItem);
-router.get('/fetchItems', adminController.fetchItems);
-router.delete('/deleteItem', adminController.deleteItem);
-router.put('/updateItem', adminController.updateItem);
+// Route to check email existence in db
+router.post("/check", adminController.check);
+router.post("/addAdmin", adminController.addAdmin);
+router.post("/addUser", adminController.addUser);
+router.post("/addItem", adminController.addItem);
+router.get("/fetchItems", adminController.fetchItems);
+router.delete("/deleteItem", adminController.deleteItem);
+router.put("/updateItem", adminController.updateItem);
 
 module.exports = router;
