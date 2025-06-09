0834f6a8f8643af5fbf85c785ddb782e24337ad8
Ackerman <mca24_1560ics@met.edu>
2025-06-09 22:07:33 +0530

    Added CollegeId in Clerk PublicMetadata

--- client/src/pages/Student.jsx
+++ client/src/pages/Student.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useUser } from "@clerk/clerk-react";
 
 const Student = () => {
+  const { user, isSignedIn } = useUser();
   return (
     <div>
       <h2>Welcome to Student Account !!!</h2>
+      <h3>Role: {user?.publicMetadata?.role}</h3>
+      <h3>College Id: {user?.publicMetadata.college_id}</h3>
     </div>
   );
 };
