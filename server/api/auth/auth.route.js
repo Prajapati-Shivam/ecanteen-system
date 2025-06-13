80e0bd2c486e5aa4f2dd2a70c9a9c966557f6dc5
DirrtyCoder <siddharthsurve29@gmail.com>
2025-06-14 00:15:02 +0530

    updated browseOrder Api

--- server/api/auth/auth.route.js
+++ server/api/auth/auth.route.js
@@ -9,6 +9,7 @@ router.post("/addAdmin", authController.addAdmin);
 router.post("/addUser", authController.addUser);
 router.post("/addOrder", authController.addOrder);
 router.post("/displayOrder", authController.displayOrder);
+router.post("/browseOrder", authController.browseOrder);
 // router.post('/set-role', authController.setRole);
 
 module.exports = router;
