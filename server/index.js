630d72898fde64420f3dbf66a6e7a44d736d433d
Ackerman <mca24_1560ics@met.edu>
2025-06-14 19:16:16 +0530

    Admin - FetchOrder API #2

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ connectDB(); // Connect to MongoDB
 // Middleware to parse incoming JSON and enable CORS
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5174",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true,
 }));
 app.use("/api", apiRoutes); // Use API routes
